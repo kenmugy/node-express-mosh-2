4d92e1f6ce5b4f51126b60e539aac2099dbcda4d
kenmugy <kenmugy@gmail.com>
2019-09-02 13:47:42 +0300

    added put request functionality also refactored that validation in a function

--- app.js
+++ app.js
@@ -13,6 +13,22 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const checkValidity = prop => {
+  const schema = {
+    name: Joi.string()
+      .min(4)
+      .required()
+  };
+  const result = Joi.validate(prop, schema);
+  const { error } = result;
+  if (error) {
+    const {
+      details: [{ message }]
+    } = error;
+    return message;
+  }
+};
+
 // get methods
 app.get('/', (req, res) => {
   res.send('this is the index page');
@@ -32,26 +48,23 @@ app.get('/api/courses/:id', (req, res) => {
 // post stuff
 app.post('/api/courses', (req, res) => {
   // joi schema
-  const schema = {
-    name: Joi.string()
-      .min(4)
-      .required()
-  };
-  const result = Joi.validate(req.body, schema);
-  const { error } = result;
-  if (error) {
-    const {
-      details: [{ message }]
-    } = error;
-    return res.status(400).send(message);
-  }
-
+  const message = checkValidity(req.body);
+  if (message) res.status(400).send(message);
   const course = {
     id: courses.length + 1,
     name: req.body.name
   };
   courses.push(course);
   res.status(201).send(course);
+});
+
+// implementing put stuff
+app.put('/api/courses/:id', (req, res) => {
+  const course = courses.find(course => course.id === parseInt(req.params.id));
+  const message = checkValidity(req.body);
+  if (message) res.status(400).send(message);
+  course.name = req.body.name;
+  res.send(course);
 });
 
 app.listen(port, () => debug(`listening on port ${chalk.green(port)}`));
