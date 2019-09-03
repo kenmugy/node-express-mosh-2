fdd69acd22436ca134a4d18f0619ae3218eb2c2e
kenmugy <kenmugy@gmail.com>
2019-09-03 10:25:46 +0300

    created routes directory to handle the routing of the app

--- app.js
+++ app.js
@@ -2,15 +2,14 @@ const express = require('express');
 const morgan = require('morgan');
 const debug = require('debug')('app');
 const chalk = require('chalk');
-const Joi = require('joi');
 
 const app = express();
 const port = process.env.PORT || 4004;
 const courses = require('./courses');
-const coursesRouter = require('./coursesRoutes')(courses);
+const coursesRouter = require('./routes/coursesRoutes')(courses);
 
 // middleware
-app.use(morgan('dev'));
+app.get('env') === 'development' && app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api/courses', coursesRouter);
